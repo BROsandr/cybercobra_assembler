036aa7e8df33bbddff0a3a951f3fc872e95d99dc
alexkharl <sasha_xarlamov@mail.ru>
2024-03-07 15:45:16 +0300

    Fix:Различные фиксы

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <vector>
+#include <bitset>
 
 int main() {
   Reader reader{std::cin};
--- test/test_compiler.cpp
+++ test/test_compiler.cpp
@@ -1,5 +1,7 @@
 #include "compiler.hpp"
 
+#include <iostream>
+
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch_test_macros.hpp"
 
@@ -8,21 +10,21 @@ TEST_CASE("Compiler compile", "[COMPILE]") {
   SECTION("empty line") {
     std::stringstream in{
         R"""(add x1, x2, x3
-
         add x1, x2, x3)"""
     };
 
     std::stringstream out{};
-    std::stringstream error{};
 
-    compile(in, out, error);
+    Compiler compiler{};
 
-    std::string line{};
+    for (std::string str; getline(in, str);) out << compiler.compile_str(str) << "\n";
+
+    Uxlen line{};
     out >> line;
-    REQUIRE(line == "00010000000010000110000000000001");
+    REQUIRE(line == 0b00010000000010000110000000000001);
     line = {};
     out >> line;
-    REQUIRE(line == "00010000000010000110000000000001");
+    REQUIRE(line == 0b00010000000010000110000000000001);
     line = {};
     out >> line;
     REQUIRE(!out);
