7b8af6c73f4b4c8b7b235f6a4f69fcd7cebb3cbc
alexkharl <sasha_xarlamov@mail.ru>
2024-03-07 15:23:25 +0300

    Ref(compiler):Вынесение в класс

--- inc/compiler.hpp
+++ inc/compiler.hpp
@@ -2,29 +2,16 @@
 
 #include "decoder.hpp"
 #include "encoder.hpp"
-#include "exception.hpp"
-#include "reader.hpp"
 
-#include <bitset>
+class Compiler {
+  public:
+    Uxlen compile_str(const std::string &line) {
 
-template <typename I, typename W, typename E>
-void compile(I &&in_stream, W &&out_stream, E &&error_stream) {
-  Decoder decoder{};
-  Encoder encoder{};
-
-  std::string line{};
-
-  Reader reader{std::forward<I>(in_stream)};
-
-  try {
-    while (getline(reader, line)) {
-      if (line.empty()) continue;
-      Decoder::Instruction_info instr_info{decoder.decode(std::move(line))};
-      Uxlen instruction{encoder.encode(instr_info)};
-      out_stream << std::bitset<32>(instruction) << "\n";
+      Decoder::Instruction_info instr_info{decoder.decode(line)};
+      return encoder.encode(instr_info);
     }
-  } catch (const Errors::Error &exc) {
-    error_stream << "ERROR at line " << std::to_string(reader.get_current_line_number()) + " :\n"
-                 << "  " << exc.what();
-  }
-}
+
+  private:
+    Decoder decoder{};
+    Encoder encoder{};
+};
